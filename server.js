471409f618708b51bd864f62875df8288749bceb
rvemula2710 <168924314+rvemula2710@users.noreply.github.com>
2026-03-26 10:28:52 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const API_KEY = process.env.API_KEY;
+
+app.get('/news', async (req, res) => {
+  const category = req.query.category || '';
+  const q = req.query.q || '';
+
+  let url = '';
+
+  if (q) {
+    url = `https://newsapi.org/v2/everything?q=${q}&apiKey=${API_KEY}`;
+  } else {
+    url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${API_KEY}`;
+  }
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  res.json(data);
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
