fa62c4c6bcdad23d943898e11f9fab2a4de39fca
rvemula2710 <168924314+rvemula2710@users.noreply.github.com>
2026-03-26 11:31:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,4 +19,6 @@ console.log(data);
   res.json(data);
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
